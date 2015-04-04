536206f9ee04e986c52e85fd8b66caa26991ab93
Kevin Pfab <kevin.pfab@gmail.com>
2015-04-04 06:16:58 +0000

    Initial commit, includes very basic command routing for on and off.

---
+++ server.js
@@ -0,0 +1,56 @@
+
+var hue = require("node-hue-api"),
+    lightState = hue.lightState;
+
+
+var host = "192.168.1.2",
+    username = "newdeveloper",
+    hueApi = new hue.HueApi(host, username);
+
+var handleLightCommand = function(command) {
+    var state = lightState.create();
+
+    if (command.match(/(turn.*on|on)/)) {
+        state = state.on();
+    } else if (command.match(/(turn.*off|off)/)) {
+        state = state.off().transition(0);
+    }
+
+    if (command.match(/brightness/)) {
+        if (command.match(/full|up|max/)) {
+            state = state.brightness(100);
+        } else if (command.match(/half|down/)) {
+            state = state.brightness(50);
+        }
+    }
+
+    setLightState(state);
+};
+
+var setLightState = function(state) {
+    for (var i = 1; i <= 3; i++) {
+        hueApi.setLightState(i, state, function(err, lights) {
+            if (err) {
+                console.log(err);
+            }
+        });
+    }
+};
+
+
+var express = require('express');
+var app = express();
+
+app.get('/command', function(req, res) {
+    var command = req.query.command;
+    console.log("New Command: " + command);
+
+    if (command.match(/light|lights/)) {
+        handleLightCommand(command);
+    }
+
+    res.status(200).end();
+});
+
+console.log('Starting on port 8001');
+app.listen(8001);
