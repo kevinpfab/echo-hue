999cb5bd44d6cd002e2a99e9595b04bebef30a09
Kevin Pfab <kevin.pfab@gmail.com>
2015-04-04 16:00:04 +0000

    Added large list of colors and their corresponding RBG values.

---
+++ README.md
@@ -0,0 +1,1 @@
+Credit to (Matthew Brush)[https://github.com/codebrainz/color-names] for the JSON color file.
--- server.js
+++ server.js
@@ -1,7 +1,18 @@
 
+// {{{ prepare color hash
+var fs = require('fs');
+var file = fs.readFileSync('colors.json', 'utf8').replace(/,}/g, '}');
+var d_colors = JSON.parse(file);
+var colors = {};
+for (var key in d_colors) {
+    var c = d_colors[key];
+    colors[c.name.toLowerCase()] = c;
+}
+
+// }}}
+
 var hue = require("node-hue-api"),
     lightState = hue.lightState;
-
 
 var host = "192.168.1.2",
     username = "newdeveloper",
@@ -17,10 +28,18 @@ var handleLightCommand = function(command) {
     }
 
     if (command.match(/brightness/)) {
-        if (command.match(/full|up|max/)) {
+        if (command.match(/full|up|max|raise/)) {
             state = state.brightness(100);
-        } else if (command.match(/half|down/)) {
+        } else if (command.match(/half|down|lower/)) {
             state = state.brightness(50);
+        }
+    }
+
+    if (command.match(/color|colored/)) {
+        for (var c in colors) {
+            if (command.indexOf(c) !== -1) {
+                state = state.rgb(colors[c].rgb);
+            }
         }
     }
 
